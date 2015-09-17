9f6c5ef9f1383bc31f5d763a6159e31464237643
DeadMansMarch <lsp425@gmail.com>
2015-09-16 21:21:09 -0400

    Update Main.clj

--- Main.clj
+++ Main.clj
@@ -1,1 +1,39 @@
 
+(ns testing.core)
+
+(defn Exp
+  [X]
+  (* X X)
+  )
+
+(defn Comp-Area
+  [Base Hight]
+  (/ (* Base Hight),2)
+  )
+
+(defn DistanceForm
+  [X,Y,A,B]
+  
+  (Math/sqrt (+ (Exp (- X A)) (Exp (- Y B))))
+  )
+
+(defn SqrCube
+  [Int]
+  (if (even? Int) (* Int Int) (* Int Int Int))
+  
+  )
+
+(defn GetDiscrim
+  [a b c]
+  
+  (- (Exp b) (* 4 a c))
+  
+  )
+
+(defn GetRealRoots
+  [X,Y,Z]
+  (let [X,(GetDiscrim X Y Z)])
+  (if (= X 0) (println "One [Real]")(if (> X 0) (println "Two [Real]") (if (< X 0) (println "2 [Complex]"))))
+  )
+
+(GetRealRoots -4 10 0)
