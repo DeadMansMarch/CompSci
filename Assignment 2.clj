d441ff033b9dc719a63f197af91c36f72838040b
DeadMansMarch <lsp425@gmail.com>
2015-10-05 12:35:42 -0400

    Update Assignment 2.clj

--- Assignment 2.clj
+++ Assignment 2.clj
@@ -43,3 +43,37 @@
   )
 
 (VectorQuadratic [[1 5 1] [3 8 12]])
+
+
+;;;;;
+
+(ns estsss.core)
+
+(defn fifthpower
+  [Num]
+  (* Num Num Num Num Num))
+
+(defn raisetofifth
+  [Vec]
+  (map fifthpower Vec)
+  )
+
+(defn raisetofifthandadd
+  [Vec]
+  (apply + (raisetofifth Vec))
+ )
+
+(defn Mean 
+  [Vec]
+  ( / (reduce #(+ %1 %2) Vec) (count Vec)
+  ))
+
+(defn Testor 
+  []
+ (fifthpower 4)
+ (raisetofifth [123 123 1234 53 2])
+ (raisetofifthandadd [123 123 1234 53 2])
+ (Mean [1 2 3 4 5 6 7])
+ )
+
+(Testor)
