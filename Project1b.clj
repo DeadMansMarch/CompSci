d1d9f03634e2ed0a348aeac29899ed11601fb33c
DeadMansMarch <lsp425@gmail.com>
2015-11-30 12:34:29 -0500

    Update Project1b.clj

--- Project1b.clj
+++ Project1b.clj
@@ -1,7 +1,17 @@
-(ns project1.core)
+(ns test.core)
 
-(def Stats {})
-(def CurrentStat "none")
+(defn Save
+  [Map]
+  (spit "StatCapture.txt" Map)
+  )
+
+(def Stats {:default [1 2 3 4 5]})
+
+(def Stats (read-string (slurp "StatCapture.txt")))
+
+(println Stats)
+
+(def CurrentStat "default")
 
 (defn Help
   []
@@ -14,7 +24,10 @@
   (println "    stdev - Calculate the standard deviation of the set.")
   (println "    help - Print the help menu.")
   (println "    five - Print the five number summary.")
+  (println "    use ds - Change the current dataset to 'ds'.")
+  (println "    save - Save the program.")
   (println "    quit - End the program."))
+
 (require '[clojure.string :as str])
 
 (defn BreakString
@@ -45,18 +58,18 @@
   (def CurrentStat (keyword String)))
 
 (defn DefineStat
-  []
-  (println "Input a data name and a data set.")
-  (let [String (BreakString (read-line)) Name (keyword (nth String 0)) Data (rest String)]
-    (def Stats (assoc Stats Name (read-string Data)))
+  [Name,Data]
+  (let [Data (map #(read-string %1) Data)]
+    (def Stats (assoc Stats Name (vec Data)))
     (SetAsStat Name)
-    (println "Stat added!")))
+    (println "Stat added :" Name ", " Data)))
 
 
 (defn KeyReader
   [UIn]
+
   (let [Stat (get Stats CurrentStat)]
-    (case (clojure.string/lower-case (read-string UIn))
+    (case (str/lower-case (read-string UIn))
       "help" (Help)
       "mean" (println "Mean :" (Mean Stat))
       "median" (println "Median :" (Median Stat))
@@ -65,7 +78,14 @@
       "prints" (println "Sorted Stat :" (sort Stat))
       "stdev" (println "Standard Deviation :" (Stdev Stat))
       "five" (println (first (sort Stat)) () (Median Stat) () (last (sort Stat)))
-     (println "Not a valid command."))
+      "save" (Save Stats)
+      "add" (let [S (rest (BreakString UIn))]
+              (DefineStat (first S) (vec (rest S))))
+      
+      "use" (SetAsStat (last (BreakString UIn)))
+      
+      "current" (println CurrentStat)
+     )
     ))
   
 
@@ -74,14 +94,13 @@
 (defn Load
   []
   (println "Welcome to SSS [Simple Statistics System].")
-  (DefineStat)
+  (println "Using default.")
   (Help)
   (doseq
     [next (take-while 
             #(and (not= %1 "quit") (not= %1 "null"))
             (repeatedly #(read-line)))]
     
-    (println (get Stats CurrentStat))
     (if (not= (get Stats CurrentStat) "nil") 
       (KeyReader next) 
       (println "Set not valid."))))
