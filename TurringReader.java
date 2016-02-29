48bab9de40367cece17a199168c32a96b5f372d1
DeadMansMarch <lsp425@gmail.com>
2016-02-29 12:35:26 -0500

    Create TurringReader.java

---
+++ TurringReader.java
@@ -0,0 +1,15 @@
+
+package virtualmachine;
+
+public class TurringReader {
+    
+    char InputStrip[];
+    
+    TurringReader(char InputStrip[]){
+        this.InputStrip = InputStrip;
+    }
+    
+    void DoRead(){
+        
+    }
+}
