50e6e39be93e778a86a5d7f28a33411249097427
DeadMansMarch <lsp425@gmail.com>
2016-02-29 12:35:43 -0500

    Update VirtualMachine.java

--- VirtualMachine.java
+++ VirtualMachine.java
@@ -1,1 +1,17 @@
 
+
+package virtualmachine;
+
+public class VirtualMachine {
+    
+    static void InitMachine(){
+        char Read[] = {'1','0','0','0','r'};
+        TurringReader Reader = new TurringReader(Read);
+    }
+    
+    public static void main(String[] args) {
+       VirtualMachine Turring = new VirtualMachine();
+       Turring.InitMachine();
+    }
+    
+}
