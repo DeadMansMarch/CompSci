68c84168e4fb9c32b5c6bb21786997fdcc08e997
DeadMansMarch <lsp425@gmail.com>
2016-03-06 22:40:39 -0500

    Create project2b.class.java

---
+++ Project2B/project2b.class.java
@@ -0,0 +1,9 @@
+package StatsAgain;
+
+public class Project2A {
+    public static void main(String[] args) {
+        System.out.println("Running stats interface.");
+        ActionInterface UserInterface = new ActionInterface();
+        UserInterface.Run();
+    }
+}
